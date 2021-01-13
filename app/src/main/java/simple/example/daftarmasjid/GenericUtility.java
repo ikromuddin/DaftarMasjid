5ec60eaa00f13b0b935c52e6fbdbd7948f32e849
ikromuddin <ikromuciha65@gmail.com>
2021-01-13 13:53:30 +0700

    Daftar Masjid

---
+++ app/src/main/java/simple/example/daftarmasjid/GenericUtility.java
@@ -0,0 +1,9 @@
+package simple.example.daftarmasjid;
+
+import java.text.SimpleDateFormat;
+
+public class GenericUtility {
+
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MMM-yyyy");
+
+}
